f0df8bce14769b373b39ec7389ec28dbc778d14f
D.V.Kavya Sai <d.v.kavyasai@gmail.com>
2024-05-03 00:18:41 +0530

    Final files added

--- index.js
+++ index.js
@@ -4,118 +4,50 @@ import pg from "pg";
 import bcrypt from "bcrypt";
 import session from "express-session";
 import passport from "passport";
-import {Strategy} from  'passport-local';
+import {Strategy} from  "passport-local";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
-const saltRounds = 15;
+env.config();
+const saltRounds = process.env.SALT_ROUNDS;
 
-
-// CHANGE PASSWORD
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "FrugalFundsMonitor",
-  password: "pencil",
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD, //password
   port: 5432,
 });
 db.connect();
-
-let transactions_demo = [{
-  "name" : "Bank Interest",
-  "type" : "income",
-  "amount" : 1000,
-  "date" : "07/01/2024",
-  "category" : "income"
-},
-{
-  "name" : "Rent",
-  "type" : "expense",
-  "amount" : 3000,
-  "date" : "07/01/2024",
-  "category" : "housing"
-},
-{
-  "name" : "Chicken Biryani",
-  "type" : "expense",
-  "amount" : 300,
-  "date" : "07/01/2024",
-  "category" : "Food"
-},
-{
-  "name" : "Salary",
-  "type" : "income",
-  "amount" : 15000,
-  "date" : "07/01/2024",
-  "category" : "income"
-},
-{
-  "name" : "Groceries",
-  "type" : "expense",
-  "amount" : 1000,
-  "date" : "07/01/2024",
-  "category" : "Groceries"
-},
-{
-  "name" : "Auto fare",
-  "type" : "expense",
-  "amount" : 500,
-  "date" : "07/01/2024",
-  "category" : "Transport"
-}];
-
-const categories_demo = [{
-  "name" : "Food",
-  "limit" : 1000
-},
-{
-  "name" : "Shopping",
-  "limit" : 2000
-},
-{
-  "name" : "Transport",
-  "limit" : 800
-},
-{
-  "name" : "Income",
-  "limit" : "-"
-},
-{
-  "name" : "Others",
-  "limit" : "-"
-}];
-
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use(session({
-  secret: "topsecret",
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
     maxAge: 1000 * 60 * 60 * 24
   }
-}))
+}));
 
 // after session use passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 // functions
-//retuens array of transactions of the specified username
+//returns array of transactions of the specified username
 async function getTransactions(username){
-  const result = await db.query("SELECT transaction_name AS name, transaction_type AS type, transaction_amount AS amount, TO_CHAR(transaction_date, 'DD-MM-YYYY') AS date, category_name AS category FROM (SELECT transactions.transaction_name, transactions.transaction_type, transactions.transaction_amount, transactions.transaction_date, transactions.username, categories.category_name FROM transactions JOIN categories ON transactions.category_id = categories.category_id) AS records WHERE records.username =$1 ORDER BY date DESC;",[username]);
+  const result = await db.query("SELECT transaction_name AS name, transaction_type AS type, transaction_amount AS amount, TO_CHAR(transaction_date, 'DD-MM-YYYY') AS date, category_name AS category FROM (SELECT transactions.transaction_name, transactions.transaction_type, transactions.transaction_amount, transactions.transaction_date, transactions.username, categories.category_name FROM transactions JOIN categories ON transactions.category_id = categories.category_id ORDER BY transaction_date DESC) AS records WHERE records.username =$1 ;",[username]);
   return result.rows;  
 }
-
 // returns the an oject containing username, balance, expense of the specified username
 async function getBalAndExp(username){
   const income = await db.query("SELECT SUM(transaction_amount) FROM transactions WHERE username =$1 AND transaction_type='income';",[username]);
   let userIncome = income.rows[0].sum;
-  const expense = await db.query("SELECT SUM(transaction_amount) FROM transactions WHERE username =$1 AND transaction_type='expense';",[username]);
+  const expense = await db.query("SELECT SUM(transaction_amount) FROM transactions WHERE username =$1 AND transaction_type='expense' AND DATE_TRUNC('month', transaction_date) = DATE_TRUNC('month', CURRENT_DATE);",[username]);
   let userExpense = expense.rows[0].sum;
   let result = {
     "username" : username,
@@ -124,13 +56,12 @@ async function getBalAndExp(username){
   };
   return result;
 }
-
 //returns a list of category objects of the specified username
 async function getCategories(username){
   const result = await db.query("SELECT category_id AS id, category_name AS name, category_limit AS limit FROM categories WHERE categories.username =$1 ORDER BY name;",[username]);
   return result.rows;
 }
-
+//gets the category of the specified category name and user
 async function getCategoryId(categoryName, username){
   const result = await db.query("SELECT category_id FROM categories WHERE category_name=$1 and username=$2;",[
     categoryName,
@@ -138,7 +69,6 @@ async function getCategoryId(categoryName, username){
   ]);
   return result.rows[0];
 }
-
 //returns the current month transactions array and previous transactions array in an array
 async function getCurrentAndPreviousTransactions(username){
   let currentMonthTransactions = [];
@@ -162,16 +92,46 @@ async function getCurrentAndPreviousTransactions(username){
   result.push(previousTransactions);
   return result;
 }
+// gets category wise expenses for the current month as data for pie chart
+async function getPieChartData(username){
+  const result = await db.query("SELECT C.category_name AS category, SUM(T.transaction_amount) AS total_expense FROM transactions T JOIN categories C ON T.category_id = C.category_id WHERE T.username=$1 AND DATE_TRUNC('month', T.transaction_date) = DATE_TRUNC('month', CURRENT_DATE) AND T.transaction_type='expense' GROUP BY C.category_name;",[
+    username
+  ]);
+  // if(result.rows.length > 0){
+    return result.rows;
+  // }else{
+  //   return -1;
+  // }
+}
+// check is the  total expenses of a certain category is under the limit or not
+async function isAmountUnderLimit(username, categoryId){
+  const result1= await db.query("SELECT category_limit FROM categories WHERE category_id = $1",[
+    categoryId
+  ]);
+  let categoryLimit = result1.rows[0].category_limit;
+  if(categoryLimit != null){
+    const result2 = await db.query("SELECT C.category_name AS category, SUM(T.transaction_amount) AS total_expense FROM transactions T JOIN categories C ON T.category_id = C.category_id WHERE T.username=$1 AND DATE_TRUNC('month', T.transaction_date) = DATE_TRUNC('month', CURRENT_DATE) AND T.transaction_type='expense' AND T.category_id=$2 GROUP BY C.category_name;",[
+      username,
+      parseInt(categoryId)
+    ]);
+    let categoryExpenses = result2.rows[0].total_expense;
+    if(parseInt(categoryExpenses) > parseInt(categoryLimit)){
+      return false;
+    }else{
+      return true;
+    }
+  }else{
+    return true;
+  }
+}
 
-// async function deleteCategory(categoryId){
-//   const result = await db.query("SELECT category_name FROM categories WHERE category_id = $1",[categoryId]); 
-// }
-
-// GET to /home - render the home page with recent 5 transactions, categories list, balance, expenses
+//user GET routes
+app.get("/",(req,res) => {
+  res.redirect("/login");
+})
 app.get("/home",async (req,res)=>{
-  console.log("at /home",req.user);
-  const user = req.user;
   if(req.isAuthenticated()){
+    const user = req.user;
     let transactions = [];
     let categories = []
     try{
@@ -182,7 +142,6 @@ app.get("/home",async (req,res)=>{
       }
       try{
         categories = await getCategories(user.username);
-        console.log(categories);
       }catch(err){
         console.log("Error getting categories", err);
         res.render("index.ejs",{
@@ -207,7 +166,6 @@ app.get("/home",async (req,res)=>{
     res.redirect("/login");
   }
 });
-
 app.get("/history",async (req,res)=>{
   if(req.isAuthenticated()){
     let currentMonthTransactions = [];
@@ -228,7 +186,6 @@ app.get("/history",async (req,res)=>{
     res.redirect("/login");
   }
 });
-
 app.get("/category",async (req,res)=>{
   if(req.isAuthenticated()){
     let categories = [];
@@ -248,31 +205,53 @@ app.get("/category",async (req,res)=>{
     res.redirect("/login");
   }
 });
+app.get("/account",(req,res)=>{
+  if(req.isAuthenticated()){
+    res.render("account.ejs",{
+      "user": req.user
+    });
+  }else{
+    res.redirect("/login");
+  }
+});
+app.get("/reports",async (req,res)=>{
+  if(req.isAuthenticated()){
+    try{
+      const pieData = await getPieChartData(req.user.username);
+      console.log(JSON.stringify(pieData));
+      if(pieData != -1){
+        res.render("reports.ejs",{
+          pieData: JSON.stringify(pieData),
+          categoryData: pieData
+        });
+      }else{
+        res.render("reports.ejs",{
+          "message": "No transactions have been made in current month to generate reports."
+        })
+      }
+    }catch(err){
+      console.log("Error getting data for plots",err);
+      res.render("reports.ejs",{
+        "errorMessage": "There was an error generating report, try again later."
+      })
+    }
+  }else{
+    res.redirect("/login");
+  }
+});
 
-app.get("/settings",(req,res)=>{
-  if(req.isAuthenticated()){
-    res.render("settings.ejs");
-  }else{
-    res.redirect("/login");
-  }
-});
-app.get("/reports",(req,res)=>{
-  if(req.isAuthenticated()){
-    res.render("reports.ejs");
-  }else{
-    res.redirect("/login");
-  }
-});
+// user authenticatin routes
 app.get("/login",(req,res)=>{
   res.render("login.ejs");
 });
-app.get("/",(req,res) => {
-  res.redirect("/login");
-})
 app.get("/signup",(req,res)=>{
   res.render("signup.ejs");
 });
-
+app.get('/logout', (req, res) => {
+  req.logout(() => {
+    res.redirect('/'); 
+  });
+});
 app.post("/signup",async (req,res) => {
     const name = req.body.name;
     const username = req.body.username;
@@ -300,7 +279,6 @@ app.post("/signup",async (req,res) => {
               username
             ]);
             req.login(user,(err) => {
-              console.log("after const user: ",err);
               res.redirect("/home");
             });
           }
@@ -311,15 +289,16 @@ app.post("/signup",async (req,res) => {
       res.render("/signup");
     }
 });
-
 app.post("/login",  passport.authenticate("local",{
   successRedirect:"/home",
   failureRedirect:"/login"
 }));
 
+// user POST routes
 app.post("/home", async (req, res) => {
   if(req.isAuthenticated()){
     try{
+      const user = req.user;
       const transaction = req.body;
       const username = req.user.username;
       const category = await getCategoryId(transaction.category,username);
@@ -333,8 +312,48 @@ app.post("/home", async (req, res) => {
         username,
         categoryId
       ]);
-      console.log(result.rows);
-      res.redirect("/home");
+      const isUnderLimit = await isAmountUnderLimit(username, categoryId);
+      let transactions = [];
+      let categories = [];
+      if(isUnderLimit === false){
+        try{
+          const userDetails = await getBalAndExp(user.username);
+          transactions = await getTransactions(user.username);
+          if(transactions.length >= 5){
+            transactions = transactions.slice(0,5);
+          }
+          try{
+            categories = await getCategories(user.username);
+            console.log(categories);
+          }catch(err){
+            console.log("Error getting categories", err);
+            res.render("index.ejs",{
+              "name_of_user" : user.name_,
+              "user_details" : userDetails,
+              "recent_transactions" : transactions,
+              "showReminder" : true,
+              "categoryExceeded" : transaction.category
+            });
+          }
+          res.render("index.ejs",{
+            "name_of_user" : user.name_,
+            "user_details" : userDetails,
+            "recent_transactions" : transactions,
+            "categories" : categories,
+            "showReminder" : true,
+            "categoryExceeded" : transaction.category
+          });
+        }catch(err){
+          console.log("Error getting transactions ",err);
+          res.render("index.ejs",{
+            "name_of_user" : user.name_,
+            "showReminder" : true,
+            "categoryExceeded" : transaction.category
+          });
+        }
+      }else{
+        res.redirect("/home");
+      }
     }catch(err){
       console.log(err);
       res.redirect("/home")
@@ -343,12 +362,9 @@ app.post("/home", async (req, res) => {
     res.redirect( "/login" );
   }
 });
-
 app.post("/add-category", async (req,res) =>{
   if(req.isAuthenticated()){
     let username = req.user.username;
-    console.log(req.body);
-    console.log(typeof(req.body.limit));
     const result =  await getCategories(username);
     let allCategories = [];
     result.forEach((item) => {
@@ -370,7 +386,6 @@ app.post("/add-category", async (req,res) =>{
     res.redirect("/login");
   }
 });
-
 app.post("/edit-category", (req,res) => {
   if(req.isAuthenticated()){
     console.log(req.body);
@@ -379,7 +394,6 @@ app.post("/edit-category", (req,res) => {
     res.redirect("/login");
   }
 });
-
 app.post("/delete-category", async (req,res) =>{
   if(req.isAuthenticated()){
     let categoryId = req.body.id;
@@ -387,7 +401,7 @@ app.post("/delete-category", async (req,res) =>{
       await db.query("DELETE FROM categories WHERE category_id=$1", [categoryId]);
       res.redirect("/category");
     }catch(err){
-      console.log(err) 
+      console.log("Can not delete category.") 
       res.redirect("/home");
     }
   }else{
@@ -395,6 +409,7 @@ app.post("/delete-category", async (req,res) =>{
   }
 })
 
+// passport strategy for user authentication
 passport.use(new Strategy(async function verify(username, password, cb){
   console.log("In strategy");
   try {
@@ -403,9 +418,6 @@ passport.use(new Strategy(async function verify(username, password, cb){
     ]);
     if (result.rows.length > 0) {
       const user = result.rows[0];
-      console.log(username);
-      console.log(password);
-      console.log(user);
       const storedHashedPassword = user.userpassword;
       bcrypt.compare(password, storedHashedPassword, (err, result) => {
         if (err) {
@@ -424,14 +436,13 @@ passport.use(new Strategy(async function verify(username, password, cb){
   } catch (err) {
     return cb(err);
   }
-}))
-
+}));
 passport.serializeUser((user,cb) =>{
   cb(null,user);
-})
+});
 passport.deserializeUser((user,cb) =>{
   cb(null,user);
-})
+});
 
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
